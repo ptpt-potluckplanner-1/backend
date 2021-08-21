96de9053de18763d5d9c574ef7c69ddc99a7d88c
PVigar88 <Pvigar@gmail.com>
2021-08-21 00:04:31 -0400

    added share config to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,30 @@
+require("dotenv").config();
+const sharedConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: {
+    directory: "./data/seeds",
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+  },
+};
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/potluckplanner.db3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
+    ...sharedConfig,
+    connection: { filename: "./data/potluckplanner.db3" },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: "./data/testing.db3" },
+  },
+  production: {
+    ...sharedConfig,
+    connection: { filename: "./data/potluckplanner.db3" },
   },
 };
