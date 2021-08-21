cf4459138522664c7d6a6ec28fa7640ff5311fc4
PVigar88 <Pvigar@gmail.com>
2021-08-21 15:46:36 -0400

    adjusted info returned

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -25,9 +25,10 @@ router.post("/login", (req, res, next) => {
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = tokenBuilder(user);
-
+        const userDeet = { user_id: user.user_id, username: user.username };
         res.status(200).json({
           message: `welcome, ${user.username}!`,
+          userDeet,
           token,
         });
       } else {
