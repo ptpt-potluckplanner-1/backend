4dfd0e598687122a036b93282f31f49f5799e493
PVigar88 <Pvigar@gmail.com>
2021-08-24 00:54:32 -0400

    finished? item model

--- api/items/items-model.js
+++ api/items/items-model.js
@@ -3,11 +3,13 @@ const db = require("../../data/dbConfig");
 async function addItem(potluck_id, item) {
   const newItem = { potluck_id: potluck_id, item: item };
 
-  await db("items").insert(newItem);
+  const [id] = await db("potluck_items").insert(newItem);
+
+  return findItemById(id);
 }
 
 function findItemById(item_id) {
-  return db("items").select("item").where({ item_id }).first();
+  return db("potluck_items").select("item").where({ item_id }).first();
 }
 
 async function findPotluckItems(potluck_id) {
@@ -16,8 +18,23 @@ async function findPotluckItems(potluck_id) {
     .where({ potluck_id: potluck_id });
 }
 
+// need function for guest claiming item.
+
+async function guestItemClaim(potluck_id, guest_id, item_id) {
+  const claimedItem = {
+    potluck_id: potluck_id,
+    guest_id: guest_id,
+    item_id: item_id,
+  };
+
+  await db("potluck-guest-item").insert(claimedItem);
+
+  return findItemById(id);
+}
+
 module.exports = {
   addItem,
   findPotluckItems,
   findItemById,
+  guestItemClaim,
 };
